38f098822c2b30d948a95f162a5c7f76807b3722
Apnamovi <99548630+Apnamovi@users.noreply.github.com>
2022-02-12 22:30:45 +0530

    Update Script.py

--- script.py
+++ script.py
@@ -1,9 +1,9 @@
 class Script(object):
 
-    START_MSG = """<b>Hy {},
+    START_MSG = """<b>Hello {} How Are You 💝,
 
 I'm an advanced filter bot with many capabilities!
-There is no practical limits for my filtering capacity :)
+Created By @ApnaMovie4
 
 See <i>/help</i> for commands and more details.</b>
 """
